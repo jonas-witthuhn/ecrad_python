4239ad300d3b947e5c0ba6870df12fb47ff5c989
Jonas Witthuhn <witthuhn@tropos.de>
2021-06-01 15:57:33 +0200

    added pip install ability

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name="ecrad_python",
+    version="0.1",
+    description="Python scripts to convert CAMS input data to ecrad input and run ecrad simulations. Also functions to perturbe input for sensitivity studies.",
+    url="https://github.com/jonas-witthuhn/ecrad_python-base",
+    license="CC BY-NC",
+    author="Jonas Witthuhn",
+    author_email="witthuhn@tropos.de",
+    packages=["ecrad_python"],
+    package_dir={"":"src"},
+    install_requires=["numpy",
+                      "xarray",
+                      "netcdf4",
+                      "scipy",
+                      "pandas",
+                      "trosat-base @ git+https://github.com/hdeneke/trosat-base.git#egg=trosat-base"],
+        )
